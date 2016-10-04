1900641d4beb2b9de81f5644b9e5d6d6a75cb536
JackMcEleney <JMCELE201@caledonian.ac.uk>
2016-10-04 14:22:17 +0100

    More code

--- GameApplication/include/Common.h
+++ GameApplication/include/Common.h
@@ -24,6 +24,15 @@ using namespace std;
 #include <SDL.h>
 #include <SDL_opengl.h>
 
+#define GLM_FORCE_RADIANS
+//maths headers
+#include <glm/glm.hpp>
+using namespace glm;
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>	
+
 #include "../Utils/include/Utils.h"
 
 #endif
