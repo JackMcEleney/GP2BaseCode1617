4ce0b4481e82c9ca2d533f4f946f9558833c69c0
JackMcEleney <JMCELE201@caledonian.ac.uk>
2016-10-11 14:07:35 +0100

    Step 5

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -1,5 +1,9 @@
 #include "MyGame.h"
 
+struct Vertex
+{
+	float x, y, z;
+};
 
 MyGame::MyGame()
 {
@@ -15,11 +19,28 @@ MyGame::~MyGame()
 void MyGame::initScene()
 {
 	GameApplication::initScene();
+	Vertex verts[] = 
+	{
+		{-0.5f, -0.5f, 0.0f}
+		{0.5f, -0.5f, 0.0f}
+		{0.0f, 0.5f, 0.0f}
+	};
+	glGenBuffers(1, &m_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+	glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(Vertex), verts, GL_STATIC_DRAW);
+	glGenVertexArrays(1, &m_VAO);
+	glBindVertexArray(m_VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+		NULL);
 }
 
 void MyGame::destroyScene()
 {
 	GameApplication::destroyScene();
+	glDeleteVertexArrays(1, &m_VAO);
+	glDeleteBuffers(1, &m_VBO);
 }
 
 void MyGame::render()
